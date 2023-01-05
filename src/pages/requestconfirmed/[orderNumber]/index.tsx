7b4f5af4aae929c98aecd77112f967dd0fa2f6e6
Logan Ricard <logvnjs@gmail.com>
2023-01-05 12:09:32 -0500

    updated request sent page

--- src/pages/requestconfirmed/[orderNumber]/index.tsx
+++ src/pages/requestconfirmed/[orderNumber]/index.tsx
@@ -1,80 +1,71 @@
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
 import { trpc } from "../../../utils/trpc";
-
-const products = [
-  {
-    id: 1,
-    name: "Cold Brew Bottle",
-    description:
-      "This glass bottle comes with a mesh insert for steeping tea or cold-brewing coffee. Pour from any angle and remove the top for easy cleaning.",
-    href: "#",
-    quantity: 1,
-    price: "$32.00",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/confirmation-page-05-product-01.jpg",
-    imageAlt: "Glass bottle with black plastic pour top and mesh insert.",
-  },
-];
 
 const RequestConfirmed = () => {
   const router = useRouter();
   const orderId = String(router.query.orderNumber);
   const myOrder = trpc.order.oneOrder.useQuery({ orderId: orderId }).data;
 
-  console.log("my new Order", myOrder);
-
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-3xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
         <div className="max-w-xl">
-          <h1 className="text-base font-medium text-indigo-600">Thank you!</h1>
+          <h1 className="text-base font-medium text-indigo-600">
+            {myOrder?.language === "English" ? "Thank You!" : "Gracias!"}
+          </h1>
           <p className="mt-2 text-4xl font-bold tracking-tight sm:text-5xl">
-            It's on the way!
+            {myOrder?.language === "English"
+              ? "Request Sent!"
+              : "¡Solicitud Enviada!"}
           </p>
           <p className="mt-2 text-base text-gray-500">
-            Your order #14034056 has shipped and will be with you soon.
+            {myOrder?.language === "English"
+              ? "You will be contacted about order"
+              : "Serás contactado sobre el pedido"}{" "}
+            #{myOrder?.id.substring(0, 7).toUpperCase()}.
           </p>
-
-          <dl className="mt-12 text-sm font-medium">
-            <dt className="text-gray-900">Tracking number</dt>
-            <dd className="mt-2 text-indigo-600">51547878755545848512</dd>
-          </dl>
         </div>
 
         <div className="mt-10 border-t border-gray-200">
           <h2 className="sr-only">Your order</h2>
 
           <h3 className="sr-only">Items</h3>
-          {products.map((product) => (
+          {myOrder?.products.map((product) => (
             <div
               key={product.id}
               className="flex space-x-6 border-b border-gray-200 py-10"
             >
-              <img
-                src={product.imageSrc}
-                alt={product.imageAlt}
-                className="h-20 w-20 flex-none rounded-lg bg-gray-100 object-cover object-center sm:h-40 sm:w-40"
-              />
+              <div className="relative h-20 w-20 flex-none  bg-gray-100 sm:h-40 sm:w-40">
+                <Image
+                  src={product.photos[0]!.url}
+                  alt={product.photos[0]!.title}
+                  fill
+                  className="rounded-lg object-cover"
+                />
+              </div>
+
               <div className="flex flex-auto flex-col">
                 <div>
                   <h4 className="font-medium text-gray-900">
-                    <a href={product.href}>{product.name}</a>
+                    {myOrder.language === "English"
+                      ? product.titleEng
+                      : product.titleEsp}
                   </h4>
                   <p className="mt-2 text-sm text-gray-600">
-                    {product.description}
+                    {myOrder.language === "English"
+                      ? product.descriptionEng
+                      : product.descriptionEsp}
                   </p>
                 </div>
                 <div className="mt-6 flex flex-1 items-end">
                   <dl className="flex space-x-4 divide-x divide-gray-200 text-sm sm:space-x-6">
                     <div className="flex">
-                      <dt className="font-medium text-gray-900">Quantity</dt>
-                      <dd className="ml-2 text-gray-700">{product.quantity}</dd>
-                    </div>
-                    <div className="flex pl-4 sm:pl-6">
                       <dt className="font-medium text-gray-900">Price</dt>
-                      <dd className="ml-2 text-gray-700">{product.price}</dd>
+                      <dd className="ml-2 text-gray-700">$ {product.price}</dd>
                     </div>
                   </dl>
                 </div>
@@ -88,45 +79,25 @@ const RequestConfirmed = () => {
             <h4 className="sr-only">Addresses</h4>
             <dl className="grid grid-cols-2 gap-x-6 py-10 text-sm">
               <div>
-                <dt className="font-medium text-gray-900">Shipping address</dt>
+                <dt className="font-medium text-gray-900">
+                  {myOrder?.language === "English" ? "Your Name" : "Tu Nombre"}
+                </dt>
                 <dd className="mt-2 text-gray-700">
                   <address className="not-italic">
-                    <span className="block">Kristin Watson</span>
-                    <span className="block">7363 Cynthia Pass</span>
-                    <span className="block">Toronto, ON N3Y 4H8</span>
+                    <span className="block">{myOrder?.name}</span>
                   </address>
                 </dd>
               </div>
               <div>
-                <dt className="font-medium text-gray-900">Billing address</dt>
+                <dt className="font-medium text-gray-900">
+                  {myOrder?.language === "English"
+                    ? "Phone Number"
+                    : "Número de teléfono"}
+                </dt>
                 <dd className="mt-2 text-gray-700">
                   <address className="not-italic">
-                    <span className="block">Kristin Watson</span>
-                    <span className="block">7363 Cynthia Pass</span>
-                    <span className="block">Toronto, ON N3Y 4H8</span>
+                    <span className="block">{myOrder?.phone}</span>
                   </address>
-                </dd>
-              </div>
-            </dl>
-
-            <h4 className="sr-only">Payment</h4>
-            <dl className="grid grid-cols-2 gap-x-6 border-t border-gray-200 py-10 text-sm">
-              <div>
-                <dt className="font-medium text-gray-900">Payment method</dt>
-                <dd className="mt-2 text-gray-700">
-                  <p>Apple Pay</p>
-                  <p>Mastercard</p>
-                  <p>
-                    <span aria-hidden="true">••••</span>
-                    <span className="sr-only">Ending in </span>1545
-                  </p>
-                </dd>
-              </div>
-              <div>
-                <dt className="font-medium text-gray-900">Shipping method</dt>
-                <dd className="mt-2 text-gray-700">
-                  <p>DHL</p>
-                  <p>Takes up to 3 working days</p>
                 </dd>
               </div>
             </dl>
@@ -135,29 +106,22 @@ const RequestConfirmed = () => {
 
             <dl className="space-y-6 border-t border-gray-200 pt-10 text-sm">
               <div className="flex justify-between">
-                <dt className="font-medium text-gray-900">Subtotal</dt>
-                <dd className="text-gray-700">$36.00</dd>
-              </div>
-              <div className="flex justify-between">
-                <dt className="flex font-medium text-gray-900">
-                  Discount
-                  <span className="ml-2 rounded-full bg-gray-200 py-0.5 px-2 text-xs text-gray-600">
-                    STUDENT50
-                  </span>
-                </dt>
-                <dd className="text-gray-700">-$18.00 (50%)</dd>
-              </div>
-              <div className="flex justify-between">
-                <dt className="font-medium text-gray-900">Shipping</dt>
-                <dd className="text-gray-700">$5.00</dd>
-              </div>
-              <div className="flex justify-between">
                 <dt className="font-medium text-gray-900">Total</dt>
-                <dd className="text-gray-900">$23.00</dd>
+                <dd className="text-gray-900">$ {myOrder?.total}</dd>
               </div>
             </dl>
           </div>
         </div>
+        <Link href={"/products"}>
+          <button
+            type="button"
+            className="mt-12 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            {myOrder?.language === "English"
+              ? "Keep Shopping"
+              : "Seguir comprando"}
+          </button>
+        </Link>
       </div>
     </div>
   );
