8e49422c81d7dcb7e5e6c14df3bbb669ab485d9c
Logan Ricard <logvnjs@gmail.com>
2023-01-06 13:52:27 -0500

    updated bilingual modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -34,7 +34,7 @@ const Modal = ({ modalOpen, setModalOpen }: ModalProps) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-amber-50 px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-amber-50 px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-md sm:p-6">
                 <div>
                   <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-orange-100">
                     <ExclamationTriangleIcon
@@ -47,12 +47,19 @@ const Modal = ({ modalOpen, setModalOpen }: ModalProps) => {
                       as="h3"
                       className="text-lg font-medium leading-6 text-yellow-600"
                     >
-                      Check Back Soon!
+                      Check Back Soon! &#x2022; ¡Vuelve a consultar pronto!
                     </Dialog.Title>
-                    <div className="mt-2">
+                    <div className="mt-4 text-left">
                       <p className="text-sm text-yellow-500">
                         This item is currently pending sale, if it does not
-                        sale, then it will be available again.
+                        sell, it will be available again.
+                      </p>
+                      <p className="text-center text-sm text-yellow-500">
+                        &#x2022;
+                      </p>
+                      <p className="text-sm text-yellow-500">
+                        Este artículo está actualmente pendiente de venta, si no
+                        se vende, volverá a estar disponible.
                       </p>
                     </div>
                   </div>
@@ -60,10 +67,10 @@ const Modal = ({ modalOpen, setModalOpen }: ModalProps) => {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-orange-100 px-4 py-2 text-base font-medium text-yellow-600 shadow-sm hover:bg-orange-200 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 sm:text-sm"
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-orange-100 px-4 py-2 text-base font-medium text-yellow-600 shadow-sm hover:bg-orange-200  sm:text-sm"
                     onClick={() => setModalOpen(false)}
                   >
-                    Close
+                    Close &#x2022; Cerrar
                   </button>
                 </div>
               </Dialog.Panel>
